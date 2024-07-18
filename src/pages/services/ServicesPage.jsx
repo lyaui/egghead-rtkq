478b18c20fb133d6920f0e567f6b1355476ffe2f
Iris Huang <peiyu.huang@mile.cloud>
2024-07-18 18:47:09 +0800

    Migrating a fetch() Call from Redux to RTK Query with builder.query()

--- src/pages/services/ServicesPage.jsx
+++ src/pages/services/ServicesPage.jsx
@@ -1,39 +1,28 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { LuckyDog } from "../dogs/LuckyDog";
-import {
-  servicesLoading,
-  servicesReceived,
-  getServicesForLuckyDog,
-} from "./servicesSlice";
-import { getDogs } from "../dogs/dogsSlice";
-import { Loader } from "../../components/Loader";
-import * as api from "../../api";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { LuckyDog } from '../dogs/LuckyDog';
+import { getDogs } from '../dogs/dogsSlice';
+import { Loader } from '../../components/Loader';
+import { useGetServiceQuery } from '../../store/apiSlice.js';
 
 export function ServicesPage() {
   const dispatch = useDispatch();
-  const services = useSelector((state) => state.services.services);
-  const loading = useSelector((state) => state.services.loading);
-  const hasServices = useSelector((state) => state.services.hasServices);
+  const { data: services, isLoading } = useGetServiceQuery();
+
   const myDogs = useSelector((state) => state.dogs.myDogs);
   const hasDogs = useSelector((state) => state.dogs.hasDogs);
   const luckyDog = useSelector((state) => state.dogs.luckyDog);
-  const myServices = useSelector(getServicesForLuckyDog);
+  const myServices = services; // useSelector(getServicesForLuckyDog);
 
   useEffect(() => {
     if (!hasDogs) dispatch(getDogs());
-    if (hasServices) return;
-    dispatch(servicesLoading());
-    api.getServices().then((services) => {
-      dispatch(servicesReceived(services));
-    });
-  }, [dispatch, hasServices, hasDogs]);
+  }, [dispatch, hasDogs]);
 
   return (
-    <div className="page">
+    <div className='page'>
       <h1>Services</h1>
-      {loading || !hasServices ? (
+      {isLoading || !services ? (
         <>
           <Loader />
           <Loader />
@@ -45,16 +34,16 @@ export function ServicesPage() {
             <p>
               We are currently showing all {services.length} of our services.
               <br />
-              To see a customized list please <Link to="/dogs">add a dog</Link>.
+              To see a customized list please <Link to='/dogs'>add a dog</Link>.
             </p>
           ) : luckyDog ? (
             myServices.length > 0 ? (
               <>
                 <p>
-                  Showing{" "}
+                  Showing{' '}
                   <b>
                     {myServices.length}/{services.length}
-                  </b>{" "}
+                  </b>{' '}
                   services available for <b>{myDogs[luckyDog].name}</b>
                 </p>
                 <LuckyDog />
@@ -80,15 +69,15 @@ export function ServicesPage() {
             </>
           )}
           {myServices.map((service) => (
-            <div className="card" key={service.id}>
+            <div className='card' key={service.id}>
               <Link to={`/services/${service.id}`}>
                 <img src={service.imageSrc} alt={service.imageAlt} />
               </Link>
-              <div className="cardContents">
+              <div className='cardContents'>
                 <h3>{service.title}</h3>
                 <h4>${service.price}</h4>
                 <p>{service.description}</p>
-                <button className="btn">Add to Cart</button>
+                <button className='btn'>Add to Cart</button>
               </div>
             </div>
           ))}
--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -2,5 +2,13 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
-  endpoints: (builder) => ({}),
+  endpoints: (builder) => ({
+    getService: builder.query({
+      query: () => ({
+        url: '/services',
+      }),
+    }),
+  }),
 });
+
+export const { useGetServiceQuery } = api;
