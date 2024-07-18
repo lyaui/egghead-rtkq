48dd6242cb3aab21bf24012d21b6125f714470fc
Iris Huang <peiyu.huang@mile.cloud>
2024-07-18 18:36:11 +0800

    Define an RTK Query API with createApi

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
-import servicesReducer from "../pages/services/servicesSlice";
-import dogsReducer from "../pages/dogs/dogsSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import servicesReducer from '../pages/services/servicesSlice';
+import dogsReducer from '../pages/dogs/dogsSlice';
+import { api } from '../store/apiSlice';
 
 export const store = configureStore({
   reducer: {
     dogs: dogsReducer,
     services: servicesReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
