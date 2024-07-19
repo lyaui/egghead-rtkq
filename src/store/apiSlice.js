215164633e2909823985d238b91c4c742c2594d6
Iris Huang <peiyu.huang@mile.cloud>
2024-07-19 18:06:04 +0800

    Transforming RTK Query Data before Caching with transformResponse

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -1,5 +1,22 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+// utilities
+export function getSize(weight) {
+  weight = parseInt(weight, 10);
+  if (weight <= 10) return 'teacup';
+  if (weight <= 25) return 'small';
+  if (weight <= 50) return 'medium';
+  if (weight <= 80) return 'large';
+  if (weight <= 125) return 'x-large';
+  return 'jumbo';
+}
+
+const YEAR = 3.156e10;
+export function getAge(dob) {
+  const date = +new Date(dob);
+  return Math.floor((Date.now() - date) / YEAR);
+}
+
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
   endpoints: (builder) => ({
@@ -24,6 +41,19 @@ export const api = createApi({
       query: (id) => ({
         url: '/dogs',
       }),
+      transformResponse: (dogs) => {
+        const allDogs = {};
+
+        for (const id in dogs) {
+          const dog = dogs[id];
+          allDogs[id] = {
+            ...dog,
+            size: getSize(dog.weight),
+            age: getAge(dog.dob),
+          };
+        }
+        return allDogs;
+      },
     }),
   }),
 });
